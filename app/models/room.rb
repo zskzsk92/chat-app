ea6231dca81c4048631c0193bbb78fe17a911dc0
zskzsk92 <zsk1992on@gmail.com>
2022-12-01 16:04:33 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
 
   validates :name ,presence: true 
 
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
